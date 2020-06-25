422df08806011f6debe2558c1a661bf0aa9e71f1
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-06-25 11:04:39 -0400

    with txt and csv files for the back end

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 from .views.home import home
 from .views.text import text
 from .views.twitter import twitter
 
+
 app = Flask(__name__, instance_relative_config=True)
-app.register_blueprint(home, text, twitter)
+CORS(app)
+app.register_blueprint(home)
+app.register_blueprint(twitter)
+app.register_blueprint(text)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
--- app/views/home.py
+++ app/views/home.py
@@ -1,6 +1,137 @@
-from flask import Blueprint, render_template, make_response
+from flask import Blueprint, render_template, make_response, request, jsonify
+import json, csv, re
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 home = Blueprint('home', __name__)
+
+vader = SentimentIntensityAnalyzer()
+
+
+@home.route('/home/pos', methods=['GET'])
+# route handler function
+def get_pos_examples():
+    # use pandas to read the csv
+    df = pd.read_csv('/app/views/sample_text_1.csv')
+    df['scores'] = df['ngrams'].apply(lambda
+    ngrams: vader.polarity_scores(ngrams))
+    df = df.to_dict(orient='list')
+    return jsonify(df)
+
+
+@home.route('/home/neg', methods=['GET'])
+
+#route handler function 
+def get_neg_examples(): 
+
+    new_words={
+        'thugs': -3.4,
+        'hoodlums': -3.4,
+        'looters': -3.4,
+        'arsonists': -2.9,
+        'kindest': 3.0
+    }
+    vader.lexicon.update(new_words)
+
+
+   #use pandas to read the csv
+    df = pd.read_csv('sample_text_2.csv')
+    df['scores'] = df['ngrams'].apply(lambda ngrams: vader.polarity_scores(ngrams))
+    df = df.to_dict(orient='list')
+    return jsonify(df)
+    
+
+#defining the get route for each word and each of their score 
+@home.route('/home/pos/words', methods = ['GET'])
+
+#route handler function 
+def return_pos_words ():
+    #adds total word column to the csv
+    df = pd.read_csv('app/views/words_sample_1.csv')
+
+    #open the text file 
+    with open ('sample_text_1.txt', 'r') as infile:
+        text_analysis = [infile.read()]
+
+        print(text_analysis, text_analysis[0], 'txt analysis + text analysis [0]')
+        #read warning 
+        ordered_list = re.sub(r"[^\w]", " ",  text_analysis[0].lower()).split()
+        print(ordered_list, 'ordered_list')
+
+        #use pandas to read the csv
+        df = pd.read_csv('words_sample_1.csv')
+        df['scores'] = df['ngrams'].apply(lambda ngrams: vader.polarity_scores(ngrams))
+
+        scored_list = df.values.tolist()
+        print(scored_list, 'list')
+
+        # printing original list 
+        print ("The original list is : " + str(scored_list)) 
+        
+        # printing sort order list 
+        print ("The sort order list is : " + str(ordered_list)) 
+        
+        # using list comprehension 
+        # to sort according to other list  
+        res = [tuple for x in ordered_list for tuple in scored_list if tuple[0] == x] 
+        
+        # printing result 
+        print ("The sorted list is : " + str(res)) 
+
+        return jsonify(res)
+
+#defining the get route for each word and each of their score 
+@home.route('/home/neg/words', methods = ['GET'])
+#route handler function 
+def return_neg_words ():
+    #adds total word column to the csv
+    df = pd.read_csv('words_sample_2.csv')
+
+    new_words = {
+        'thugs': -3.4,
+        'hoodlums': -3.4,
+        'looters': -3.4,
+        'arsonists': -2.9,
+        'kindest': 3.0
+    }
+    vader.lexicon.update(new_words)
+
+
+    #open the text file 
+    with open ('sample_text_2.txt', 'r') as infile:
+        text_analysis = [infile.read()]
+
+        print(text_analysis, text_analysis[0], 'txt analysis + text analysis [0]')
+        #read warning 
+        ordered_list = re.sub(r"[^\w]", " ",  text_analysis[0].lower()).split()
+        print(ordered_list, 'ordered_list')
+
+        #use pandas to read the csv
+        df = pd.read_csv('words_sample_2.csv')
+        df['scores'] = df['ngrams'].apply(lambda ngrams: vader.polarity_scores(ngrams))
+
+        scored_list = df.values.tolist()
+        print(scored_list, 'list')
+
+        # printing original list 
+        print ("The original list is : " + str(scored_list)) 
+        
+        # printing sort order list 
+        print ("The sort order list is : " + str(ordered_list)) 
+        
+        # using list comprehension 
+        # to sort according to other list  
+        res = [tuple for x in ordered_list for tuple in scored_list if tuple[0] == x] 
+        
+        # printing result 
+        print ("The sorted list is : " + str(res)) 
+
+    return jsonify(res)
+        
 
 
 @home.errorhandler(404)
