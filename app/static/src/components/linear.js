258a642da1ecbe5023f92ef97fac0c3491f9b2f5
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-07-15 11:20:28 -0400

    with linear progression tracker

---
+++ app/static/src/components/linear.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useEffect, useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
+export default function LinearDeterminate() {
+  const classes = useStyles();
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
+    function progress() {
+      setCompleted((oldCompleted) => {
+        if (oldCompleted === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldCompleted + diff, 100);
+      });
+    }
+
+    const timer = setInterval(progress, 500);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress variant="determinate" value={completed} />
+    </div>
+  );
+}
