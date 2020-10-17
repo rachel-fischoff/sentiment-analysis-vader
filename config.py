6b13a15932ed2ad02c5a393f0a47ce4e24a1ee8d
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-10-17 00:46:02 -0400

    changed the npm build script & added serve dependency

--- config.py
+++ config.py
@@ -19,3 +19,18 @@ class StagingConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+
+
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+    import logging
+    from logging import StreamHandler
+    file_handler = StreamHandler()
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
+
+
+
